28b160853b1a4aac97f1fb0ef769d6f30d752c4c
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-24 10:34:22 +0300

    added profile signal

--- awwards/models.py
+++ awwards/models.py
@@ -20,11 +20,11 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-# @receiver(post_save, sender=User)
-# def update_profile_signal(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
 
 
 # class Project(models.Model):
