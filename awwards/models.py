2c21bcc34abd90ea1a98c1468ad607b275182241
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-24 10:33:42 +0300

    added profile model

--- awwards/models.py
+++ awwards/models.py
@@ -1,24 +1,24 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from cloudinary.models import CloudinaryField
-# import datetime as dt
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# import statistics
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+import datetime as dt
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import statistics
 
-# # Create your models here.
+# Create your models here.
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     profile_photo = CloudinaryField('profile_photo',blank=True)
-#     # profile_photo = ImageField(blank=True, manual_crop="")
-#     bio = models.TextField(max_length=500, blank=True)
-#     location = models.CharField(max_length=60, blank=True)
-#     contact = models.CharField(max_length=60,blank=True)
-#     create_at=models.DateTimeField(auto_now_add=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField('profile_photo',blank=True)
+    # profile_photo = ImageField(blank=True, manual_crop="")
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=60, blank=True)
+    contact = models.CharField(max_length=60,blank=True)
+    create_at=models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return self.user.username
+    def __str__(self):
+        return self.user.username
 
 # @receiver(post_save, sender=User)
 # def update_profile_signal(sender, instance, created, **kwargs):
