6b14600f64f8713bbcb32d0c9fd68e56781f8dd8
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-24 08:28:17 +0300

    added templates

--- awwards/admin.py
+++ awwards/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-# Register your models here.
+# from .models import Profile,Project,Rating
+
+# # Register your models here.
+
+# admin.site.register(Profile)
+# admin.site.register(Project)
+# admin.site.register(Rating)
--- awwards/apps.py
+++ awwards/apps.py
@@ -1,5 +1,6 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class AwwardsConfig(AppConfig):
-    name = 'awwards'
+# class AwwardsConfig(AppConfig):
+#      default_auto_field = 'django.db.models.BigAutoField'
+#      name = 'awwards'
