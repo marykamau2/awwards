254db580f1b46b7d566e198174e3062228d787b1
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-24 10:39:58 +0300

    registered profile model

--- awwards/admin.py
+++ awwards/admin.py
@@ -1,9 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import Profile,Project,Rating
+from .models import Profile,Project,Rating
 
-# # Register your models here.
+# Register your models here.
 
-# admin.site.register(Profile)
+admin.site.register(Profile)
 # admin.site.register(Project)
 # admin.site.register(Rating)
