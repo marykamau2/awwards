2a51e3b9d0fee5dbae895cd10933824e983c1d5a
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-24 10:37:39 +0300

    added home page view

--- awwards/views.py
+++ awwards/views.py
@@ -1,33 +1,33 @@
-# from django.shortcuts import render,redirect,reverse
-# from django.contrib.auth import authenticate, login, logout
-# from django.contrib import messages
-# from .forms import SignUpForm,ProjectForm,RatingsForm,UserProfileForm
-# from django.http  import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
-# from django.contrib.auth.decorators import login_required
-# from .models import Project,Rating,Profile
-# import datetime
-# import random
+from django.shortcuts import render,redirect,reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm,ProjectForm,RatingsForm,UserProfileForm
+from django.http  import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import Project,Rating,Profile
+import datetime
+import random
 
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-# from .serializer import ProfileSerializer,ProjectsSerializer
-# # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer,ProjectsSerializer
+# Create your views here.
 
-# def home_page(request):
-#     projects=Project.get_all_projects()
-#     today = datetime.date.today()
-#     ratings= Rating.objects.all()
-#     prjs= [p.project for p in ratings]
-#     site_of_day=random.choice(prjs)
-#     site_of_day_ratings=get_project_ratings_av(site_of_day.id)
+def home_page(request):
+    projects=Project.get_all_projects()
+    today = datetime.date.today()
+    ratings= Rating.objects.all()
+    prjs= [p.project for p in ratings]
+    site_of_day=random.choice(prjs)
+    site_of_day_ratings=get_project_ratings_av(site_of_day.id)
 
-#     context={
-#         'projects':projects,
-#         'today':today,
-#         'site':site_of_day,
-#         'site_ratings':site_of_day_ratings
-#     }
-#     return render(request,'index.html',context)
+    context={
+        'projects':projects,
+        'today':today,
+        'site':site_of_day,
+        'site_ratings':site_of_day_ratings
+    }
+    return render(request,'index.html',context)
 
 
 # def register_user(request):
