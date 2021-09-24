269e8f53e018b173c7033ff48ee4549135f539e8
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-24 10:47:29 +0300

    added homepage url

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,9 +1,9 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
 
-# urlpatterns = [
-#     path('', views.home_page,name='home_page'),
+urlpatterns = [
+    path('', views.home_page,name='home_page'),
 #     path('project/new',views.submit_project,name='submit_project'),
 #     path('search/projects',views.search_projects,name='search_projects'),
 #     path('view/project/<prj_id>',views.view_project,name='view_project'),
