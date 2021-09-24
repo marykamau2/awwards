17d4823980bc8f334b412d4d391182e7249f07a7
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-24 10:49:01 +0300

    added the register user view

--- awwards/views.py
+++ awwards/views.py
@@ -30,20 +30,20 @@ def home_page(request):
     return render(request,'index.html',context)
 
 
-# def register_user(request):
-#   if request.method == "POST":
-#     form = SignUpForm(request.POST)
-#     if form.is_valid():
-#         form.save()
-#         username = form.cleaned_data.get('username')
-#         password = form.cleaned_data.get('password1')
-#         user = authenticate(username=username, password=password)
-#         login(request, user)
-#         messages.success(request, f'Account for username {username} successfully created.')
-#         return redirect('home_page')
-#   else:
-#       form = SignUpForm()
-#   return render(request, 'registration/registration_form.html', {'form': form})
+def register_user(request):
+  if request.method == "POST":
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        messages.success(request, f'Account for username {username} successfully created.')
+        return redirect('home_page')
+  else:
+      form = SignUpForm()
+  return render(request, 'registration/registration_form.html', {'form': form})
 
   
 # def user_login(request):
