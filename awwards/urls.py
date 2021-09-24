c6aeb9c7895567e4887b2bf73ae9156b5bbc4859
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-24 10:48:09 +0300

    added project url

--- awwards/urls.py
+++ awwards/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page,name='home_page'),
-#     path('project/new',views.submit_project,name='submit_project'),
+    path('project/new',views.submit_project,name='submit_project'),
 #     path('search/projects',views.search_projects,name='search_projects'),
 #     path('view/project/<prj_id>',views.view_project,name='view_project'),
 #     path('view/my-profile',views.my_profile,name='my_profile'),
